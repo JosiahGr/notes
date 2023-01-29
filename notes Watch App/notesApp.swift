258be4bf4c70e77d9239d30220a8238c5509c4bd
Josiah Green <JosiahGreen90@gmail.com>
2023-01-28 16:48:22 -0800

    Initial Commit

---
+++ notes Watch App/notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesApp.swift
+//  notes Watch App
+//
+//  Created by Josiah Green on 1/28/23.
+//
+
+import SwiftUI
+
+@main
+struct notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
